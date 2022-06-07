01289e0e667e3c628d998983327cbb0efa5a8eac
OzodbekYaxshiboyev <107023146+ozodbekyaxshiboyev@users.noreply.github.com>
2022-06-07 11:23:16 +0500

    Anvar Narzullayev Ustozning darslari
    python asoslari

---
+++ python-darslar-main/06-numbers/javoblar-06-dars-03.py
@@ -0,0 +1,20 @@
+"""
+09/11/2020
+
+Dasturlash asoslari
+
+#06-dars: Sonlar
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+# Foydalanuvchidan ikki son kiritshni so'rab,
+# kiritilgan sonlarning yig'indisi, ayirmasi,
+# ko'paytmasi va bo'linmasini chiqaruvchi dastur
+a = float(input("Birinchi sonni kiriting: "))
+b = float(input("Ikkinchi sonni kiriting: "))
+print(f"{a}+{b}=", a + b)
+print(f"{a}-{b}=", a - b)
+print(f"{a}x{b}=", a * b)
+print(f"{a}/{b}=", a / b)
---
+++ python-darslar-main/README.md
@@ -0,0 +1,2 @@
+# python-darslar
+Assalom alaykum hurmatli Do'stlar, bu yerda <a href="https://python.sariq.dev">python.sariq.dev</a> sahifamizida qo'yib borilayotgan darslarga oid kodlar va dasturlarni yuklab olishingiz mumkin.
